0f24f3f1c918f2d378305820f37953e35fc3f3ab
Illuminatiiiiii <kinglydeveloper@gmail.com>
2020-08-05 11:46:37 -0500

    init

---
+++ src/main/java/me/kodysimpson/cortexbot/CortexBotApplication.java
@@ -0,0 +1,13 @@
+package me.kodysimpson.cortexbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CortexBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CortexBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/kodysimpson/cortexbot/CortexBotApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kodysimpson.cortexbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CortexBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
