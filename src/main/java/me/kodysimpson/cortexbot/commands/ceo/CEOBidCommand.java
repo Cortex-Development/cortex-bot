e503aaedbb9e000256134a1993363e82376bc0a3
Kody Simpson <kodysimpson0616@gmail.com>
2021-06-12 18:42:57 -0500

    no negatives

--- src/main/java/me/kodysimpson/cortexbot/commands/ceo/CEOBidCommand.java
+++ src/main/java/me/kodysimpson/cortexbot/commands/ceo/CEOBidCommand.java
@@ -40,19 +40,23 @@ public class CEOBidCommand extends Command {
         //make sure the amount is a number
         try{
             int points = Integer.parseInt(args);
-            Member member = memberRepository.findByUserIDIs(event.getMember().getId());
+            if (points > 0){
+                Member member = memberRepository.findByUserIDIs(event.getMember().getId());
 
-            //see if they have the points to back it up
-            if (member.getPoints() >= points){
+                //see if they have the points to back it up
+                if (member.getPoints() >= points){
 
-                ceoService.newBid(member, points);
+                    ceoService.newBid(member, points);
 
-                event.reply("Bid created. Thank you.");
+                    event.reply("Bid created. Thank you.");
 
-                event.getJDA().getTextChannelById("562684703073632286").sendMessage("A new bid of " + points + " point(s) for CEO was placed by " + event.getMember().getEffectiveName()).queue();
+                    event.getJDA().getTextChannelById("562684703073632286").sendMessage("A new bid of " + points + " point(s) for CEO was placed by " + event.getMember().getEffectiveName()).queue();
 
+                }else{
+                    event.reply("You can't afford that! You only have " + member.getPoints() + " point(s).");
+                }
             }else{
-                event.reply("You can't afford that! You only have " + member.getPoints() + " point(s).");
+                event.reply("Bid an amount greater than zero.");
             }
 
         }catch (NumberFormatException ex){
