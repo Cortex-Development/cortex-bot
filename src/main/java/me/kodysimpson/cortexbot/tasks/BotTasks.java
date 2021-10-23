8f407fed8ed03f986cebd116091c85f796ce459d
Kody Simpson <kodysimpson0616@gmail.com>
2021-10-23 00:32:57 -0500

    version number verif

--- src/main/java/me/kodysimpson/cortexbot/tasks/BotTasks.java
+++ src/main/java/me/kodysimpson/cortexbot/tasks/BotTasks.java
@@ -24,7 +24,7 @@ public class BotTasks {
 
     @Scheduled(fixedRate = 864000000, initialDelay = 60000)
     public void announceStart(){
-        getGuild().getTextChannelById("786974733123846214").sendMessage("Cortex bot redeployed. Version: 1.0").queue();
+        getGuild().getTextChannelById("786974733123846214").sendMessage("Cortex bot redeployed. Version: 1.1").queue();
     }
 
     @Scheduled(fixedRate = 3600000)
